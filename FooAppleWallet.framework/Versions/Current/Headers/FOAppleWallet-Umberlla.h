7fabefc1cf7806445ace0946a1ada365c31b15f4
Mac Mini Server <testios@foo-solutions.com>
2024-10-10 13:31:13 +0300

    Push Framework Version: 1.1.12

--- FooAppleWallet.framework/Versions/Current/Headers/FOAppleWallet-Umberlla.h
+++ FooAppleWallet.framework/Versions/Current/Headers/FOAppleWallet-Umberlla.h
@@ -4,12 +4,13 @@
 //
 //  Created by Charbel Hasrouni on 25/04/2024.
 //
-
-#import "FOInAppProvisioning.h"
+#import "FOInAppProtocol.h"
 #import "FOAppleWalletExtension.h"
 #import "FOInAppProvisioning.h"
 #import "FOAppleWallet.h"
+#import "FOInAppJWE.h"
 #import "FOCard.h"
 #import "FOInAppVerificationCard.h"
+#import "FOInAppProvisioning.h"
 #import "PKPaymentPassHelper.h"
 #import "FOInAppProvCardType.h"
